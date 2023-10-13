171dff9068abe94368682bdae7512930d32b060c
Patrick Herrmann <patrick@productplan.com>
2023-10-13 10:02:08 -0700

    Tweak config

--- dist/cjs/shrink-to-fit-widthShrinkToFitWidth.cy.js
+++ dist/cjs/shrink-to-fit-widthShrinkToFitWidth.cy.js
@@ -1,6 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
+const react_1 = __importDefault(require("react"));
 const shrink_to_fit_width_1 = require("./shrink-to-fit-width");
 describe("ShrinkToFitWidth", () => {
     it("renders", () => {
